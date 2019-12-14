78081536f7d0117b66db7020f910799aba8cba15
student <tester@tester-m>
2019-12-14 13:53:24 +0100

    wszystkie testy, do sprawdzenia

--- test.py
+++ test.py
@@ -5,12 +5,23 @@ class test_strings(unittest.TestCase):
 
     def test_kapitaliki(self):
         wynik = capitalize.capitalize()
-        self.assertEqual(wynik, 'Ala Ma Kota')
+        self.assertEqual(wynik, 'Ala ma kota')
 
     def test_liczenie(self):
         wynik = capitalize.count('kota')
         self.assertEqual(wynik, 1)
 
+    def test_alfabet(self):
+        wynik = capitalize.isalpha()
+        self.assertEqual(wynik, 'True')
+
+    def test_cyfry(self):
+        wynik = capitalize.isdigit()
+        self.assertEqual(wynik, 'False')
+
+    def test_male_litery(self):
+        wynik = capitalize.islower()
+        self.assertEqual(wynik, 'True')
 
 if __name__ == '__main__':
     unittest.main()
